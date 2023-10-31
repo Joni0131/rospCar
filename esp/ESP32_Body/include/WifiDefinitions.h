1a926c65f146f3a222b60a8685c5c68d9042288f
Joni0131 <Jonas.BergerFEU@web.de>
2023-10-31 17:22:53 +0100

    Default Wifi settings

---
+++ esp/ESP32_Body/include/WifiDefinitions.h
@@ -0,0 +1,9 @@
+#ifndef WIFIDEFINITIONS_H
+#define WIFIDEFINITIONS_H
+
+//TODO: If you wanna use it you have to configure those three parameters
+#define SSID "Your_Wifi_SSID"
+#define PASSWORD "Your_Wifi_Password"
+#define IPADDRESS xxx,xxx,xxx,xxx //IPv4 Address of the recieving device in this format
+
+#endif
